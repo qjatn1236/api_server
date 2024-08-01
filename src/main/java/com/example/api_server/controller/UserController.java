20283d1ca455f453f8ddd50bf0207b62d7ed9635
김범수 <qjatn1236@naver.com>
2024-08-01 11:45:01 +0900

    CORS

--- src/main/java/com/example/api_server/controller/UserController.java
+++ src/main/java/com/example/api_server/controller/UserController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -29,6 +30,7 @@ import lombok.extern.slf4j.Slf4j;
 @RestController
 @Slf4j
 @RequiredArgsConstructor
+@CrossOrigin(origins = "https://api-server-xvqy.onrender.com/")
 @Tag(name = "User", description = "User management APIs")
 public class UserController {
 	private final UserRepository userRepository;
